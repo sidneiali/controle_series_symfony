1b47abbe5372039b96968173cb3b58d1e50fe214
Sidnei Izaias <sidnei.izaias@melius.software>
2023-06-25 20:48:59 -0300

    views

---
+++ src/Controller/SeriesController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SeriesController extends AbstractController
+{
+    #[Route('/series', name: 'app_series', methods: ['GET'])]
+    public function index(): Response
+    {
+
+        $seriesList = [
+            'Lost',
+            'Grey\'s Anatomy',
+            'Loki',
+            'Suits',
+        ];
+
+        return $this->render('series/index.html.twig', [
+            'seriesList' => $seriesList,
+        ]);
+    }
+
+    #[Route('/series/create', methods: ['GET'])]
+    public function addSeriesForm(): Response
+    {
+        return $this->render('/series/form.html.twig');
+    }
+}
